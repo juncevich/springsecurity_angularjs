9fba1ebb0ead8ad459d6f5b18f8de5b9d220d1e1
Alexandr Juncevich <a.juncevich@gmail.com>
2017-06-15 17:48:25 +0500

    Add cors to resource controller

--- resource/src/main/java/com/example/demo/controller/SampleResourceController.java
+++ resource/src/main/java/com/example/demo/controller/SampleResourceController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +15,8 @@ import com.example.demo.dto.Message;
 public class SampleResourceController {
 
     @RequestMapping("/")
+    @CrossOrigin(origins = "*",
+            maxAge = 3600)
     public Message home() {
 
         return new Message("Hello World");
