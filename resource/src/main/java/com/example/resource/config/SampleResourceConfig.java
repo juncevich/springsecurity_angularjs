e63b723034c92d13fb5ed946447b8d65c74835b2
Alexandr Juncevich <a.juncevich@gmail.com>
2017-06-15 22:25:09 +0500

    Add security config to resource

--- resource/src/main/java/com/example/resource/config/SampleResourceConfig.java
+++ resource/src/main/java/com/example/resource/config/SampleResourceConfig.java
@@ -1,8 +1,10 @@
 package com.example.resource.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.session.web.http.HeaderHttpSessionStrategy;
 
 /**
  * Resource security config
@@ -14,5 +16,11 @@ public class SampleResourceConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 
         http.cors().and().authorizeRequests().anyRequest().authenticated();
+    }
+
+    @Bean
+    HeaderHttpSessionStrategy sessionStrategy() {
+
+        return new HeaderHttpSessionStrategy();
     }
 }
