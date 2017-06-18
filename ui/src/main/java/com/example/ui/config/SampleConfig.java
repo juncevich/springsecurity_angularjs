e1639e9a1fa28d5a7a5325a5a4cdb42e7021860a
Alexandr Juncevich <a.juncevich@gmail.com>
2017-06-18 18:57:02 +0500

    Change ui

--- ui/src/main/java/com/example/ui/config/SampleConfig.java
+++ ui/src/main/java/com/example/ui/config/SampleConfig.java
@@ -1,37 +1,23 @@
 package com.example.ui.config;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.client.OAuth2ClientContext;
-import org.springframework.security.oauth2.client.OAuth2RestTemplate;
-import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 /**
  * Security config
  */
 @Configuration
-@EnableZuulProxy
 @EnableOAuth2Sso
 public class SampleConfig extends WebSecurityConfigurerAdapter {
 
     @Override
-    public void configure(HttpSecurity http) throws Exception {
+    protected void configure(HttpSecurity http) throws Exception {
 
-        http.authorizeRequests().antMatchers("/index.html", "/home.html", "/").permitAll()
-                .anyRequest().authenticated().and().csrf()
-                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+        http.httpBasic().and().authorizeRequests().antMatchers("/index.html", "/").permitAll()
+                .anyRequest().hasRole("USER");
+
     }
 
-    @Bean
-    protected OAuth2RestTemplate oAuth2RestTemplate(OAuth2ProtectedResourceDetails resource,
-            @Qualifier("oauth2ClientContext") OAuth2ClientContext context) {
-
-        return new OAuth2RestTemplate(resource, context);
-    }
 }
--- ui/src/main/java/com/example/ui/controller/SampleController.java
+++ ui/src/main/java/com/example/ui/controller/SampleController.java
@@ -1,5 +1,10 @@
 package com.example.ui.controller;
 
+import java.security.Principal;
+import java.util.Collections;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -8,4 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SampleController {
 
+    @RequestMapping("/user")
+    public Map<String, String> user(Principal user) {
+
+        return Collections.singletonMap("name", user.getName());
+    }
 }
