4c030d942e43ebb5b453f507113679d7f9ff0cd8
Alexandr Juncevich <a.juncevich@gmail.com>
2017-06-18 19:02:16 +0500

    Change resource

--- resource/src/main/java/com/example/resource/config/SampleResourceConfig.java
+++ resource/src/main/java/com/example/resource/config/SampleResourceConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 /**
  * Resource security config
@@ -15,8 +16,7 @@ public class SampleResourceConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        http.httpBasic().disable();
-        http.authorizeRequests().anyRequest().authenticated();
+        http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
     }
 
 }
