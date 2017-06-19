eec939f167b30229eb4886bec313ee41d168de32
Alexandr Juncevich <a.juncevich@gmail.com>
2017-06-19 22:10:44 +0500

    Fix some mistakes

--- ui/src/main/java/com/example/ui/config/SampleConfig.java
+++ ui/src/main/java/com/example/ui/config/SampleConfig.java
@@ -1,6 +1,8 @@
 package com.example.ui.config;
 
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -8,6 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
  * Security config
  */
 @Configuration
+@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 public class SampleConfig extends WebSecurityConfigurerAdapter {
 
     @Override
