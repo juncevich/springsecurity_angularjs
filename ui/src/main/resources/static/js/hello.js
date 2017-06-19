717e9504e60717471b23e2fa4ca43c2c30f944c7
Alexandr Juncevich <a.juncevich@gmail.com>
2017-06-20 00:24:22 +0500

    Fix some mistakes

--- ui/src/main/resources/static/js/hello.js
+++ ui/src/main/resources/static/js/hello.js
@@ -1,43 +1,22 @@
-angular.module('hello', [ 'ngRoute' ]).config(function($routeProvider) {
+angular.module('hello', []).controller('home', function($http) {
 
-    $routeProvider.when('/', {
-        templateUrl : 'home.html',
-        controller : 'home',
-        controllerAs : 'controller'
-    }).otherwise('/');
-
-}).controller('navigation',
-
-    function($rootScope, $http, $location, $route) {
-
-        var self = this;
-
-        self.tab = function(route) {
-            return $route.current && route === $route.current.controller;
-        };
-
-        $http.get('user').then(function(response) {
-            if (response.data.name) {
-                $rootScope.authenticated = true;
-            } else {
-                $rootScope.authenticated = false;
-            }
-        }, function() {
-            $rootScope.authenticated = false;
-        });
-
-        self.credentials = {};
-
-        self.logout = function() {
-            $http.post('logout', {}).finally(function() {
-                $rootScope.authenticated = false;
-                $location.path("/");
-            });
-        }
-
-    }).controller('home', function($http) {
     var self = this;
-    $http.get('resource/').then(function(response) {
-        self.greeting = response.data;
-    })
+    console.log('Loading');
+    $http.get('user').then(function(response) {
+
+        var data = response.data;
+        if (data.name) {
+            self.authenticated = true;
+            self.user = data.name
+            $http.get('/resource/').then(function(response) {
+
+                self.greeting = response.data;
+            })
+        } else {
+            self.authenticated = false;
+        }
+    }, function() {
+
+        self.authenticated = false;
+    });
 });
