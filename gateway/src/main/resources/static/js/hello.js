76830f8283001be8cfaa609c72e57b79f142c219
Alexandr Juncevich <a.juncevich@gmail.com>
2017-06-21 21:52:03 +0500

    Add code to hello.js

--- gateway/src/main/resources/static/js/hello.js
+++ gateway/src/main/resources/static/js/hello.js
@@ -1,3 +1,16 @@
 /**
- * Created by juncevich on 6/21/17.
+ * Hello component
+ * 
+ * Created by Aleksandr Juncevich on 6/21/17.
  */
+angular.module('hello', [ 'ngRoute', 'home', 'navigation' ]).config(function($routeProvider, $httpProvider) {
+
+    $routeProvider.when('/', {
+            templateUrl: 'js/home/home.html',
+            controller: 'home'
+    }).when('/login', {
+            templateUrl: 'js/navigation/login.html',
+            controller: 'navigation'
+    }).otherwise('/');
+    $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+});
