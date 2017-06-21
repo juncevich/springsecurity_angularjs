f9a6d9ea8ab50bfdeb61c26195448be4ae70be1a
Alexandr Juncevich <a.juncevich@gmail.com>
2017-06-21 21:48:47 +0500

    Add code to home.js

--- gateway/src/main/resources/static/js/home/home.js
+++ gateway/src/main/resources/static/js/home/home.js
@@ -1,3 +1,13 @@
 /**
+ * Home component
+ * 
  * Created by juncevich on 6/21/17.
  */
+angular.module('home', []).controller('home', function($http) {
+
+    var self = this;
+    $http.get('/user/').then(function(response) {
+
+        self.user = response.data.name;
+    });
+});
